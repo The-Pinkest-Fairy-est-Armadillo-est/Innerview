42439030871b256bfc933d96c1ba19119d7089b2
stphnl <stphn.l.nyc@gmail.com>
2023-01-23 10:38:50 -0500

    modifications to post.js to accept form data

--- client/containers/post.js
+++ client/containers/post.js
@@ -1,36 +1,128 @@
 // import modules
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-// define a 'Post' React functional component
-const clickHandler = (e) => {
-    console.log('hi');
-    console.log(e.target.parentNode.value);
+const Post = props => {
+
+    const navigate = useNavigate();
+    const companyNameRef = useRef();
+    const roleRef = useRef();
+    const locationRef = useRef();
+    const cultureRef = useRef();
+    const questionsRef = useRef();
+    const challengesRef = useRef();
+    const descriptionRef = useRef();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(companyNameRef.current.value);
+        console.log(roleRef.current.value);
+        console.log(locationRef.current.value);
+        console.log(cultureRef.current.value);
+        console.log(questionsRef.current.value);
+        console.log(challengesRef.current.value);
+        console.log(descriptionRef.current.value);
+        navigate('/');
+    }
+    
+    return (
+        <div className = 'postPage'>
+            <h1 className = 'postHeader'>Post Interview</h1> 
+            <form className = 'postContents' id = 'usrform' onSubmit={handleSubmit}>
+                <label 
+                    htmlFor = "companyName"
+                    className = 'input-line'
+                >Company Name:
+                </label>
+                <textarea
+                    name='Company Name' 
+                    className = 'sm-input-box' 
+                    form='usrform' 
+                    placeholder = 'Write company name here.' 
+                    ref={companyNameRef}>
+                </textarea>
+                <label
+                    htmlFor = "role" 
+                    className = 'input-line'
+                >Role/position:
+                </label>
+                <textarea 
+                    name='Role/position' 
+                    className = 'sm-input-box' 
+                    form='usrform' 
+                    placeholder = 'Write role/position here.'
+                    ref={roleRef}>
+                </textarea>
+                <label 
+                    htmlFor = "location" 
+                    className = 'input-line'
+                >Location:
+                </label>
+                <textarea 
+                    name='Location' 
+                    className = 'sm-input-box' 
+                    form='usrform' 
+                    placeholder = 'Write location here.' 
+                    ref={locationRef}>
+                </textarea>
+                <label 
+                    htmlFor = "culture" 
+                    className = 'input-line'
+                >Culture Sense:
+                </label>
+                <textarea 
+                    name='Culture Sense' 
+                    className = 'sm-input-box' 
+                    form='usrform' 
+                    placeholder = "First impressions based on culture." 
+                    ref={cultureRef}>
+                </textarea>
+                <label 
+                    htmlFor = "questions"
+                    className = 'input-line'
+                >Behavioral / Non-Technical Questions:
+                </label>
+                <textarea 
+                    name='Behavioral Questions' 
+                    className = 'md-input-box' 
+                    form='usrform' 
+                    placeholder = 'Write any challenging behavioral or non-technical questions you faced that you would like to share with the class.' 
+                    ref={questionsRef}>
+                </textarea>
+                <label 
+                    htmlFor = "challenges" 
+                    className = 'input-line'
+                >Technical Challenges:
+                </label>
+                <textarea 
+                    name='Technical Challenges' 
+                    className = 'md-input-box' 
+                    form='usrform' 
+                    placeholder = 'Write any technical challenges you were tested on during your interview.' 
+                    ref={challengesRef}>
+                </textarea>
+                <label 
+                    htmlFor = "description"
+                    className = 'input-line'
+                >Interview Description:
+                </label>
+                <textarea 
+                    name='Interview Description' 
+                    className = 'lg-input-box' 
+                    form='usrform' 
+                    placeholder = 'Provide additional details regarding your interview here to share to your classmates.' 
+                    ref={descriptionRef}>
+                </textarea>
+                <button
+                    type = 'submit'
+                    className = 'signUpButton input-line'
+                >Submit
+                </button>
+            </form>
+        </div>
+    );
 }
-
-const Post = props => (
-    <div className = 'postPage'>
-        <h1 className = 'postHeader'>Post Interview</h1> 
-        <form className = 'postContents' id = 'usrform'>
-            <label htmlFor = "name" className = 'input-line'>Company Name:</label>
-            <textarea name='Company Name' className = 'sm-input-box' form='usrform' placeholder = 'Write company name here.'></textarea>
-            <label htmlFor = "name" className = 'input-line'>Role/position:</label>
-            <textarea name='Role/position' className = 'sm-input-box' form='usrform' placeholder = 'Write role/position here.'></textarea>
-            <label htmlFor = "name" className = 'input-line'>Location:</label>
-            <textarea name='Location' className = 'sm-input-box' form='usrform' placeholder = 'Write location here.'></textarea>
-            <label htmlFor = "name" className = 'input-line'>Culture Sense:</label>
-            <textarea name='Culture Sense' className = 'sm-input-box' form='usrform' placeholder = "First impressions based on culture."></textarea>
-            <label htmlFor = "name" className = 'input-line'>Behavioral / Non-Technical Questions:</label>
-            <textarea name='Behavioral Questions' className = 'md-input-box' form='usrform' placeholder = 'Write any challenging behavioral or non-technical questions you faced that you would like to share with the class.'></textarea>
-            <label htmlFor = "name" className = 'input-line'>Technical Challenges:</label>
-            <textarea name='Technical Challenges' className = 'md-input-box' form='usrform' placeholder = 'Write any technical challenges you were tested on during your interview.'></textarea>
-            <label htmlFor = "name" className = 'input-line'>Interview Description:</label>
-            <textarea name='Interview Description' className = 'lg-input-box' form='usrform' placeholder = 'Provide additional details regarding your interview here to share to your classmates.'></textarea>
-
-            <button type = 'submit' className = 'signUpButton input-line' onClick={clickHandler}><Link to = '/interface'>Sign in</Link></button>
-        </form>
-    </div>
-);
 
 // export React component
 export default Post;
