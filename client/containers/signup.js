af6519d618541c990b82bc50d6d9f70a2e44bb8b
Lawrence Liang <lawrenceql@gmail.com>
2023-01-23 14:16:38 -0500

    signup-update law

--- client/containers/signup.js
+++ client/containers/signup.js
@@ -1,54 +1,55 @@
-import { React } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
-const Signup = props => (
+const Signup = props => {
 
-    // const nameRef = useRef();
-    // const emailRef = useRef();
-    // const passRef = useRef();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passRef = useRef();
 
-    // const clickHandler = () => {
-    //     const body = {
-    //         name: nameRef.current.value,
-    //         password: passRef.current.value,
-    //         email: emailRef.current.value,
-    //     }
+    const clickHandler = () => {
+        const body = {
+            name: nameRef.current.value,
+            password: passRef.current.value,
+            email: emailRef.current.value,
+        }
 
-    //     fetch('http://localhost:3000/signup', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'Application/JSON',
-    //         },
-    //         body: JSON.stringify(body),
-    //     })
-    //         .then(data => data.json())
-    //         .then(data => {
-    //             console.log(data)
-    //             return useNavigate('/');
-    //         })
-    //         .catch(err => {
-    //             console.log('Error POST request on Sign up submit:', err);
-    //             return prompt("Bad request");
-    //         });
-    // }
+        fetch('http://localhost:3000/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'Application/JSON',
+            },
+            body: JSON.stringify(body),
+        })
+            .then(data => data.json())
+            .then(data => {
+                console.log(data)
+                return useNavigate('/');
+            })
+            .catch(err => {
+                console.log('Error POST request on Sign up submit:', err);
+                return prompt("Bad request");
+            });
+    }
     
-    
+    return (
     <div className = 'loginPage'>
         <div className = 'loginBox'>
             <h1>Sign Up</h1>
-            {/* <form className = 'postContents'>
-                <label HtmlFor = 'Name'>Name: </label>
+            <form className = 'postContents'>
+                <label htmlFor = 'Name'>Name: </label>
                 <input type = 'text' className = 'inputBox' id = 'Name' placeholder = 'Your Name' ref = {nameRef}/>
-                <label HtmlFor = 'Email' className = 'input-line'>Email: </label>
+                <label htmlFor = 'Email' className = 'input-line'>Email: </label>
                 <input type = 'email' className = 'inputBox' id = 'Email' placeholder = 'Your Name' ref = {emailRef}/>
-                <label HtmlFor = 'Password' className = 'input-line'>Password: </label>
+                <label htmlFor = 'Password' className = 'input-line'>Password: </label>
                 <input type = 'password' className = 'inputBox' id = 'Password' placeholder = 'Your Password' ref = {passRef}/>
                 <button type = 'submit' className = 'signUpButton input-line' onSubmit = {clickHandler}><Link to = '/interface'>Sign up</Link></button>
                 <button type = 'button' className = 'signUpButton input-line'><Link to = '/'>Return home</Link></button>
-            </form> */}
+            </form>
         </div>
     </div>
-    
-);
+    )
+};
 
 export default Signup;
