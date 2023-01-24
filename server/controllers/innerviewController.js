bf3b249bb08f8922322413930727d69755c8ab65
stphnl <stphn.l.nyc@gmail.com>
2023-01-23 23:09:26 -0500

    added comments to all backend stuffs

--- server/controllers/innerviewController.js
+++ server/controllers/innerviewController.js
@@ -1,16 +1,19 @@
+// Requiring-in our 'innerviewModels' to a const so that we can use the query method
+// to query the DB 
 const db = require('../models/innerviewModels');
 
+// Creating an empty object to hold all our Middleware functions as methods
 const innerviewController = {};
 
+// 'getPeople' will return everything from the 'people' table
 innerviewController.getPeople = (req, res, next) => {
-  console.log('executing innerviewController.getPeople')
   const queryText = `
     SELECT * FROM people
   `;
 
   db.query(queryText)
     .then(results => {
-      res.locals.results = results.rows;
+      res.locals.results = results.rows; // information coming back from the DB will be found on its 'row' property
       return next();
     })
     .catch(err => next({
@@ -19,19 +22,22 @@ innerviewController.getPeople = (req, res, next) => {
     }));
 };
 
+// 'getPosts' will retrieve all the posts from the DB
 innerviewController.getPosts = (req, res, next) => {
-  console.log('executing innerviewController.getPosts')
-  // write code here
+  // We're interested in the name column from the 'people' table
+  // and almost all the columns from the 'posts' table
+  // and we want to perform a LEFT JOIN to include everything from the 'posts' table selected
+  // where the '_id' primary key in the 'people' table matches the 'people_id' foreign key in the 'posts' table
   const queryText = `
-  SELECT people.name, posts.role, posts.behavioral_questions, posts.technical_challenges, posts.sense_of_culture, posts.interview_description, posts.company_name, posts.location
-  FROM posts
-  LEFT JOIN people
-  ON people._id = posts.people_id;
+    SELECT people.name, posts.role, posts.behavioral_questions, posts.technical_challenges, posts.sense_of_culture, posts.interview_description, posts.company_name, posts.location
+    FROM posts
+    LEFT JOIN people
+    ON people._id = posts.people_id;
   `;
 
   db.query(queryText)
     .then(results => {
-      res.locals.results = results.rows;
+      res.locals.results = results.rows; // information coming back from the DB will be found on its 'row' property
       return next();
     })
     .catch(err => next({
@@ -40,16 +46,33 @@ innerviewController.getPosts = (req, res, next) => {
     }));
   };
 
+// 'postPosts' will add a post entry to the 'posts' table in the DB
 innerviewController.postPosts = (req, res, next) => {
-  const { email, role, behavioral_questions, technical_challenges, sense_of_culture, interview_description, company_name, location } = req.body;
+  // deconstructing the information coming in from the req.body (Post Interview form submission)
+  const { 
+    email, 
+    role, 
+    behavioral_questions, 
+    technical_challenges, 
+    sense_of_culture, 
+    interview_description, 
+    company_name, 
+    location 
+  } = req.body;
 
+  // Creating our first query here, the purpose is to find the person's ID on the 'people' table
+
+  // since we are only provided the 'email' value of the person making the post in the req.body
+  // we will need to find the person's id so that we can update the 'people' table entry accordingly
   const firstQuery = `
     SELECT _id
     FROM people
     WHERE email='${email}';
   `;
 
+  // creating a varible 'people_id' to hold our found person ID
   let people_id;
+  // Query the DB to find the person's '_id', and save it into the 'people_id' variable
   db.query(firstQuery)
     .then(result => people_id = result)
     .catch(err => next({
@@ -57,20 +80,32 @@ innerviewController.postPosts = (req, res, next) => {
       message: { err:  `Issue ocurred when trying to post. Please try again later.`},
     }));
 
+  // Now we can set up our 'params' array (paramaterizing our queries), including the 'people_id' variable to
+  // Fill in that column on the 'posts' table
   const params = [ people_id, role, behavioral_questions, technical_challenges, sense_of_culture, interview_description, company_name, location ];
+  
+  // Some error handling here, we're checking if all fields have been filled in
+  // if not, the 'params' array will have an empty string as one of its elements
   if (params.includes('')) {
-    res.locals.results = 'empty field'; //send this string to front end not all fields are filled
+    // we will send this string to front end as a flag that not all fields are filled
+    res.locals.results = 'empty field';
+    // and end this request response cycle
     return next();
   };
 
+  // If we got here, this means all fields were filled out
+  // and we can create a query to update our 'posts' table
   const queryText = `
     INSERT INTO posts (people_id, role, behavioral_questions, technical_challenges, sense_of_culture, interview_description, company_name, location)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
   `;
   
+  // Query the DB to insert this new post to the 'posts' table
   db.query(queryText, params)
     .then(() => {
+      // we will send this string to front end as a flag that the form was successfully posted
       res.locals.results = 'successfully posted';
+      // and end the request response cycle
       return next();
     })
     .catch(err => next({
@@ -79,18 +114,30 @@ innerviewController.postPosts = (req, res, next) => {
     }));
 };
 
+// 'signUp' will add a user to the DB
+// this function is async as it depends on a fetch request to resolve before moving on
+// with the JS thread of execution of the remainder of the code
 innerviewController.signUp = async (req, res, next) => {
+  // Deconstruct the req.body properties here
   const { name, password, email } = req.body;
-  console.log('interviewController input:', name, password, email)
+
+  // And set up our 'params' array for our query
   const params = [ name, password, email ];
 
+  // But first, some error handling to see if a field was not filled out
   if (params.includes('')) {
+    // we will send this string to front end as a flag that the form was not all filled out
     res.locals.user = 'missingInfo';
+    // and end the request response cycle
     return next();
   }
 
+  // We'll now have to check if the user is already existing
+  // create a variable 'existingUser' to act as a flag, and set it as false
   let existingUser = false;
 
+  // Create a query to check our 'people' table in the DB
+  // to see if the email exists
   const emailQueryCheck = `
     SELECT name
     FROM people
@@ -98,7 +145,10 @@ innerviewController.signUp = async (req, res, next) => {
   `;
 
   await db.query(emailQueryCheck)
+    // if the email exists, it will be included in the 'rows' property of the result
     .then(results => {
+      // which in this case, the 'rows' value, which is an array, will have length
+      // and we will set the 'existingUser' flag to true
       if (results.rows.length) existingUser = true;
     })
     .catch(err => next({
@@ -106,11 +156,16 @@ innerviewController.signUp = async (req, res, next) => {
       message: { err:  `Issue ocurred when trying to sign up. Please try again later.`},
     }));
 
+  // If the user exists, then 
   if (existingUser) {
+    // we will send this string to front end as a flag that the user already exists
     res.locals.user = 'userexists';
+    // and end the request response cycle
     return next();
   }
   else {
+    // otherwise, the user does not exist, and we can make a query to add them
+    // to the DB
     const addUserQuery = `
       INSERT INTO people (name, password, email)
       VALUES ($1, $2, $3)
@@ -118,7 +173,9 @@ innerviewController.signUp = async (req, res, next) => {
 
     db.query(addUserQuery, params)
       .then(() => {
+        // we will send this string to front end as a flag that the user has been created
         res.locals.user = 'userAdded';
+        // and end the request response cycle
         return next();
       })
       .catch(err => next({
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,23 +1,31 @@
 const express = require('express');
-
+// Requiring-in the 'innerviewController' object so that we have access to our
+// Middleware functions to handle any logic within our routes
 const innerviewController = require('../controllers/innerviewController');
-
+// 
 const router = express.Router();
 
+// Handling the GET route for the 'sign-in' button on the login form
+// for front-end validation
 router.get('/', innerviewController.getPeople, (req, res) => {
   return res.status(200).json(res.locals.results);
 });
 
+// Handling the GET route for after logging in, retrieving all posts from the DB
+// to display on the interface page
 router.get('/posts', innerviewController.getPosts, (req, res) => {
   return res.status(200).json(res.locals.results);
 });
 
+// Handling the POST route for the 'submit' button on the post interview form
 router.post('/', innerviewController.postPosts, (req, res) => {
   return res.status(200).json(res.locals.results);
 });
 
+// Handling the POST route for the 'sign-up' button on the login form
 router.post('/signup', innerviewController.signUp, (req, res) => {
   return res.status(200).json(res.locals.user);
 })
 
+// Exporting the 'router' object so that it can be used in 'server.js'
 module.exports = router;
